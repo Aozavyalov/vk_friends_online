4ed86a59eb5d1e8b742496f8f3e4a26d5300fe0b
Andrew <Aozavyalov@protonmail.com>
2017-03-22 11:58:31 +0300

    Added a separation to online and online_mobile in output

--- get_friends_online.py
+++ get_friends_online.py
@@ -73,14 +73,14 @@ def online_friends_output(online_friends_ids, token):
     if 'error' in founded_friends_ids:
         print(founded_friends_ids['error']['error_msg'])
         sys.exit()
-    online_friend = []
+    online_friends = {}
     for mode in online_friends_ids:
         # doesn't work with list(
         # vk.users.get(user_ids=online_friends_ids[mode], v='5.63')
 
         # works, but slowly
         # for user_id in online_friends_ids[mode]:
-        #     online_friend.extend(vk.users.get(user_ids=user_id, v='5.63'))
+        #     online_friends.extend(vk.users.get(user_ids=user_id, v='5.63'))
 
         # So, there is only one way...
         # Use make_request_to_vk because if i send to request.get params list with ids,
@@ -89,11 +89,17 @@ def online_friends_output(online_friends_ids, token):
                                              parameters={'user_ids': online_friends_ids[mode], 'v': '5.63',
                                                          'access_token': token})
         response = requests.get(url=url_for_request).json()
-        part_of_friends = response['response']
-        online_friend.extend(part_of_friends)
+        online_friends[mode] = response['response']
 
-    for friend in online_friend:
-        print(friend['first_name'], friend['last_name'], 'https://vk.com/id%d' % friend['id'])
+    for mode in online_friends:
+        if mode == 'online':
+            print('Online:')
+        elif mode == 'online_mobile':
+            print('Online from mobile:')
+        else:
+            print('Something else:')
+        for friend in online_friends[mode]:
+            print(friend['first_name'], friend['last_name'], 'https://vk.com/id%d' % friend['id'])
 
 
 if __name__ == '__main__':
